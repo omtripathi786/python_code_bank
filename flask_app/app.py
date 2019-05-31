81c290c56d69eb4475a78abb8e95f8f91c5597bf
Tripathi <om.tripathi@accenture.com>
2019-05-31 17:34:57 +0530

    adding some flask code in section4

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,6 +2,14 @@ from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
+import oneagent
+import oneagent.sdk as onesdk
+
+if not oneagent.initialize():
+    print('Error initializing OneAgent SDK.')
+
+getsdk = oneagent.get_sdk
+
 stores = [{
     'name': 'My Store',
     'items': [{'name': 'my item', 'price': 15.99}]
@@ -35,7 +43,10 @@ def get_store(name):
 
 @app.route('/stores/')
 def get_stores():
-    return jsonify({'stores': stores})
+    sdk = getsdk()
+
+    with sdk.trace_custom_service('my_fancy_transaction', 'MyFancyService'):
+        return jsonify({'stores': stores})
 
 
 @app.route('/store/<string:name>/item', methods=['POST'])
@@ -49,3 +60,5 @@ def get_items_in_store(name):
 
 
 app.run(port=5000)
+
+oneagent.shutdown()
--- flask_app/section4/app.py
+++ flask_app/section4/app.py
@@ -3,3 +3,24 @@ from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+
+items = []
+
+
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        else:
+            return {'error': 'Item not found'}
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item
+
+
+api.add_resource(Item, '/item/<string:name>')
+
+app.run(port=5000)
